c9b84ffa0de95531ac98d1f549295b37ae48b969
Henry Page <dev@henrypage.com>
2026-02-07 13:24:19 +0100

    Change antigen installation

--- config/zsh/setup-antigen.zsh
+++ config/zsh/setup-antigen.zsh
@@ -23,7 +23,7 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle supercrabtree/k
-antigen bundle 1henrypage/zsh-treehouse
+antigen bundle 1henrypage/zsh-treehouse --branch=main
 
 # Apply Antigen
 antigen apply
